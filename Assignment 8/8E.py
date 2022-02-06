d7ada9ba1ae369dae1f025084bdd241aa71354f1
Harsh Baid <74610712+am-here@users.noreply.github.com>
2022-02-06 16:30:22 +0530

    Update 8E.py

--- Assignment 8/8E.py
+++ Assignment 8/8E.py
@@ -1,5 +1,5 @@
 def middle_3(original):
-    if(len(original) % 2 == 0 or len(original) <= 7):
+    if(len(original) % 2 == 0 or len(original) < 7):
         return "Invalid Input"
     ind = (len(original)-3)//2
     return original[ind:ind+3]
